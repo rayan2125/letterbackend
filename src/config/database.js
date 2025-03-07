26de503e17be06c23e595c1666e2c1ae959bdc38
rayan2125 <dhananjay.s.dubey20125@gmail.com>
2025-03-07 17:10:51 +0530

    all debugging done with token probelm solve --done

--- src/config/database.js
+++ src/config/database.js
@@ -5,18 +5,18 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-// const sequelize = new Sequelize(process.env.DATABASE_URL, {
-//   dialect: "postgres",
-//   dialectOptions: {
-//     ssl: {
-//       require: true,
-//       rejectUnauthorized: false,
-//     },
-//   },})
-const sequelize = new Sequelize("mydatabase", "postgres", "your_password", {
-  host: "localhost",
-  dialect: "postgres", // Specifies PostgreSQL
-  logging: false, // Disable logging (optional)
-});
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },})
+// const sequelize = new Sequelize("mydatabase", "postgres", "your_password", {
+//   host: "localhost",
+//   dialect: "postgres", // Specifies PostgreSQL
+//   logging: false, // Disable logging (optional)
+// });
 
 export default sequelize;
